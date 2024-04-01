af55597851ffdcc7963271a43ab8e15a2b53399b
huangjie4 <1364045672@qq.com>
2024-04-01 13:51:46 +0800

    修改哦！！！

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="content">
-		<text class="iconfont icon-shoucang font-md"></text>
+		<text class="iconfont icon-shoucang font-md" style="font-size: 200rpx"></text>
 	</view>
 </template>
 
