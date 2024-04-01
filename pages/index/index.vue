1cea20544ef4500db8cb3b4e6887ca705cd92cd9
huangjie4 <1364045672@qq.com>
2024-04-01 10:13:57 +0800

    修改

---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="content">
+		fdsfdf
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
