885e38446c5f79c8fcfc00c43caacacc530b76ef
huangjie4 <1364045672@qq.com>
2024-04-01 16:38:45 +0800

    修改

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
-	<view class="flex align-center justify-center mx-2 border rounded">
-		<Icon icon-id="icon-tubiao11" icon-size="25" icon-color="text-light-muted"></Icon>
-		<input type="text" placeholder="搜索你想听的节目" class="flex-1"/>
+	<view class="flex align-center justify-center mx-2 border rounded bg-light search-title">
+		<Icon icon-id="icon-tubiao11" icon-size="25" icon-color="text-light-muted" class="p-1"></Icon>
+		<input type="text" placeholder="搜索你想听的节目" class="flex-1 font-sm"/>
 	</view>
 </template>
 
@@ -25,5 +25,9 @@
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+.search-title{
+	height: 62rpx;
+	opacity: 0.8;
+}
 </style>
