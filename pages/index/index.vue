d7e919bf233371b455eb44b7fd0cba5c39eb9c78
huangjie4 <1364045672@qq.com>
2024-04-02 14:43:33 +0800

    修改

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,19 +3,7 @@
 		<SearchBox></SearchBox>
 		<RotationChart :image-list="imgArr"></RotationChart>
 		<FunctionSort :function-sort="functionSort"></FunctionSort>
-		<ListHeader>
-			<template #title>猜你喜欢</template>
-			<template #tips>更多推荐</template>
-		</ListHeader>
-		<view class="flex align-center">
-			<block v-for="(item,index) in rebooks" :key="index" class="rebook-image">
-				<view class="flex-1 rebook-image flex-column flex align-center position-relative">
-					<image :src="item.src" class="rebook-image"></image>
-					<text class="font my-1 text-light-black">{{ item.name }}</text>
-					<icon icon-id="icon-erji" icon-color="text-light-white" icon-size="30" class="position-absolute er-ji"></icon>
-				</view>
-			</block>
-		</view>
+		<Recommond :rebooks="rebooks"></Recommond>
 	</view>
 </template>
 
@@ -23,7 +11,7 @@
 	import SearchBox from '@/components/SearchBox.vue'
 	import RotationChart from "@/components/RotationChart.vue"
 	import FunctionSort from "@/components/FunctionSort.vue"
-	import ListHeader from "@/components/ListHeader.vue"
+	import Recommond from "@/components/compound/Recommond.vue"
 	export default {
 		data() {
 			return {
@@ -66,26 +54,26 @@
 					 }
 				 ],
 				 rebooks:[
-					 {
-						 id:1,
-						 name:'西游记',
-						 src:'/static/guessLike/333.jpg'
-					 },
-					 {
-						 id:2,
-						 name:'贺兰山志',
-						 src:'/static/guessLike/444.jpg'
-					 },
-					 {
-						 id:3,
-						 name:'水浒传',
-						 src:'/static/guessLike/111.jpg'
-					 },
-					 {
-						 id:4,
-						 name:'文城',
-						 src:'/static/guessLike/222.jpg'
-					 }
+				  {
+				 	 id:1,
+				 	 name:'西游记',
+				 	 src:'/static/guessLike/333.jpg'
+				  },
+				  {
+				 	 id:2,
+				 	 name:'贺兰山志',
+				 	 src:'/static/guessLike/444.jpg'
+				  },
+				  {
+				 	 id:3,
+				 	 name:'水浒传',
+				 	 src:'/static/guessLike/111.jpg'
+				  },
+				  {
+				 	 id:4,
+				 	 name:'文城',
+				 	 src:'/static/guessLike/222.jpg'
+				  }
 				 ]
 			}
 		},
@@ -96,7 +84,7 @@
 			SearchBox,
 			RotationChart,
 			FunctionSort,
-			ListHeader
+			Recommond
 		},
 		methods: {
 
@@ -105,12 +93,5 @@
 </script>
 
 <style lang="scss" scoped>
-	.rebook-image{
-		width: 130rpx;
-		height: 220rpx;
-		.er-ji{
-			left: 40rpx;
-			top: 10rpx;
-		}
-	}
+	
 </style>
