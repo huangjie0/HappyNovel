48667221a9833a1f93d270982ca7d24bb08a2bb9
huangjie4 <1364045672@qq.com>
2024-04-02 11:02:25 +0800

    修改

---
+++ components/ListHeader.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="flex align-center justify-between p-2 my-2">
+		<view class="font-lg">
+			<slot name="title"></slot>
+		</view>
+		<view class="flex align-center">
+			<view class="font text-light-black">
+				<slot name="tips">查看全部</slot>
+			</view>
+			<icon icon-id="icon-iconfonti" icon-color="text-light-black"></icon>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
