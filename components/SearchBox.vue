1249a0175642514960815ed13eb37be108bae11d
huangjie4 <1364045672@qq.com>
2024-04-02 11:05:03 +0800

    修改

---
+++ components/SearchBox.vue
@@ -0,0 +1,17 @@
+<template>
+	<view class="flex align-center justify-center mx-2 border rounded bg-light search-title">
+		<Icon icon-id="icon-tubiao11" icon-size="25" icon-color="text-light-muted" class="p-1"></Icon>
+		<input type="text" placeholder="搜索你想听的节目" class="flex-1 font-sm"/>
+	</view>
+</template>
+
+<script>
+	
+</script>
+
+<style lang="scss" scoped>
+.search-title{
+	height: 62rpx;
+	opacity: 0.8;
+}
+</style>
