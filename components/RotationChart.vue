10b37d6230d546926a3408e235d5ca71e55e9dad
huangjie4 <1364045672@qq.com>
2024-04-02 09:43:12 +0800

    修改

---
+++ components/RotationChart.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="m-2">
+		<swiper indicator-dots autoplay circular class="swiper-conter" indicator-active-color="#ffffff">
+			<swiper-item v-for="(item,index) in imageList" :key="index">
+				<image :src="item.src" mode="aspectFill" class="w-100"></image>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default{
+		props:{
+			imageList:{
+				type:Array,
+				default:()=>[]
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.swiper-conter{
+	height: 380rpx;
+}
+</style>
