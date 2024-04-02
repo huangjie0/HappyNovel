e13a1bb8f71965fb3844bab985a105a6be8c6edc
huangjie4 <1364045672@qq.com>
2024-04-02 10:49:02 +0800

    修改

---
+++ components/FunctionSort.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="flex align-center justify-between mx-3 py-4">
+		<block v-for="(item,index) in functionSort" :key="index">			
+			<view class="flex flex-column align-center">
+				<icon :icon-id="item.iconId" :icon-color="item.iconColor" icon-size="65"></icon>
+				<text class="mt-1 font text-light-black">{{ item.name }}</text>
+			</view>
+		</block>
+	</view>
+</template>
+
+<script>
+	export default{
+		props:{
+			functionSort:{
+				type:Array,
+				default:()=>[]
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
