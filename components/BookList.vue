c98d09584b1acfadd580d5826b8902458888d136
huangjie4 <1364045672@qq.com>
2024-04-03 09:37:29 +0800

    修改

--- components/BookList.vue
+++ components/BookList.vue
@@ -1,40 +1,29 @@
 <template>
 	<view>
-		<block v-for="(item,index) in bookList" :key="index">
-			<ListHeader>
-				<template #title>
-					{{ item.headerTitle }}
-				</template>
-			</ListHeader>
-			<block v-for="(mitem,mindex) in item.books" :key="mindex">
-				<view class="books-container border-bottom flex align-center p-1" hover-class="bg-light">
-					<image :src="mitem.imgurl" mode="aspectFill" class="books-image rounded mx-2"></image>
-					<view class="books-text flex-1">
-						<view class="py-2 font-md font-weight-bold">
-							{{ mitem.name }}
-						</view>
-						<view class="font text-light-black">
-							{{ mitem.synopsis }}
-						</view>
+		<block v-for="(mitem,mindex) in bookList" :key="mindex">
+			<view class="books-container border-bottom flex align-center p-1" hover-class="bg-light">
+				<image :src="mitem.imgurl" mode="aspectFill" class="books-image rounded mx-2"></image>
+				<view class="books-text flex-1">
+					<view class="py-2 font-md font-weight-bold">
+						{{ mitem.name }}
+					</view>
+					<view class="font text-light-black">
+						{{ mitem.synopsis }}
 					</view>
 				</view>
-			</block>
+			</view>
 		</block>
 	</view>
 	
 </template>
 
 <script>
-	import ListHeader from "@/components/ListHeader.vue"
 	export default{
 		props:{
 			bookList:{
 				type:Array,
 				default:()=>[]
 			}
-		},
-		components:{
-			ListHeader
 		}
 	}
 </script>
