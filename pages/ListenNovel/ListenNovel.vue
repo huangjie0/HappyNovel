18688d311bdd6620c3114127093e5c260911c09b
huangjie4 <1364045672@qq.com>
2024-04-03 09:48:30 +0800

    修改

---
+++ pages/ListenNovel/ListenNovel.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		听小说
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
