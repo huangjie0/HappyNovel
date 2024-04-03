3ddcf6875ccdfee55011d83301b5007e4293d7f6
huangjie4 <1364045672@qq.com>
2024-04-03 09:17:15 +0800

    修改

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -11,7 +11,17 @@
 				</template>
 			</ListHeader>
 			<block v-for="(mitem,mindex) in item.books" :key="mindex">
-				
+				<view class="books-container border-bottom flex align-center p-1" hover-class="bg-light">
+					<image :src="mitem.imgurl" mode="aspectFill" class="books-image rounded mx-2"></image>
+					<view class="books-text flex-1">
+						<view class="py-2 font-md font-weight-bold">
+							{{ mitem.name }}
+						</view>
+						<view class="font text-light-black">
+							{{ mitem.synopsis }}
+						</view>
+					</view>
+				</view>
 			</block>
 		</block>
 	</view>
@@ -186,5 +196,14 @@
 </script>
 
 <style lang="scss" scoped>
-
+.books-container{
+	height: 320rpx;
+	.books-image{
+		width: 250rpx;
+		height: 250rpx;
+	}
+	.books-text{
+		height: 100%;
+	}
+}
 </style>
