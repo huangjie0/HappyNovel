974ad40b6589114e356035a1eec9271d8ad34211
huangjie4 <1364045672@qq.com>
2024-04-08 14:25:41 +0800

    修改

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 import App from './App'
 import Icon from '@/components/Icon.vue'
+import store from 'store'
 
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
 Vue.config.productionTip = false
+Vue.prototype.$store = store
 App.mpType = 'app'
 const app = new Vue({
+	store,
   ...App
 })
 Vue.component('Icon',Icon)
