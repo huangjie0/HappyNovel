60746a3f4a736b935a855bf9b2d24fc0870c3f6b
huangjie4 <1364045672@qq.com>
2024-04-08 13:39:30 +0800

    修改

--- components/MyAudio.vue
+++ components/MyAudio.vue
@@ -1,6 +1,16 @@
 <template>
 	<view class="fixed-bottom audi-container mx-2 mb-1 rounded">
-		进度部分
+		<!-- 进度部分 -->
+		<view class="flex align-center justify-center font audi-slider">
+			<!-- 总时长 -->
+			<view>00:00</view>
+			<!-- 进度条部分 -->
+			<view class="slider-container">
+				<slider block-size="16" active-color="#e48267" background-color="#eef2f3"/>
+			</view>
+			<!-- 当前播放时刻 -->
+			<view>00:00</view>
+		</view>
 	</view>
 </template>
 
@@ -12,5 +22,11 @@
 		height: 160rpx;
 		background-color: #d1ccc0;
 		opacity: 0.9;
+		.audi-slider{
+			color: #7a8388;
+		}
+		.slider-container{
+			width: 500rpx;
+		}
 	}
 </style>
