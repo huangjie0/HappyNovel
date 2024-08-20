a4215d15abc5320c7f5754dd5432dcba4ac77a20
huangjie4 <1364045672@qq.com>
2024-08-20 11:00:56 +0800

    修改

--- pages/reading/reading.vue
+++ pages/reading/reading.vue
@@ -1,19 +1,18 @@
 <template>
 	<view>
+		<!-- 占位 -->
+		<view class="cal"></view>
 		<!-- 设置开始 -->
-		<view class="fixed-top bg-white shadow animated" hover-class="slideInDown">
+		<view class="fixed-top bg-white shadow animated slideInDown" v-if="setStatus">
 			<view class="reading-container flex align-center">
 				<icon icon-id="icon-jiantou-copy px-2"></icon>
 				<text>{{ novalName }} </text>
 				<text class="px-2 font-sm text-ellipsis flex-1">章节：{{ chapterCatalog[0].title }}</text>
 			</view>
 		</view>
-		<!-- 设置结束 -->
-		<!-- 中间内容区域 -->
-		
-		
+		<!-- 设置结束 -->		
 		<!-- 设置底部部分 -->
-		<view class="reading-bottom flex align-center fixed-bottom bg-white shadow font animated" hover-class="slideInUp">
+		<view class="reading-bottom flex align-center fixed-bottom bg-white shadow font animated slideInUp" v-if="setStatus">
 			<view class="flex-1 flex flex-column align-center">
 				<icon icon-id="icon-xueyuan-mulu" icon-size="55"></icon>
 				<view>目录</view>
@@ -31,22 +30,43 @@
 				<view>更多</view>
 			</view>
 		</view>
+		<!-- 文本部分 -->
+		
+		<swiper class="px-2" :style="{ height:`${calHeight}rpx`}" @tap="changeSetStatus">
+			<swiper-item v-for="(item,index) in testChapters" :key="item.id">
+				<scroll-view scroll-y :style="{ height:`${calHeight}rpx`}">
+					<rich-text :nodes="item.text"></rich-text>
+				</scroll-view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
 <script>
 	import test from '@/common/test.js'
 	import tool from '@/common/tool.js'
+	import htmlParser from '@/common/html-parser.js'
 	export default {
 		data() {
 			return {
 				novalName:test.name,  //小说姓名
 				chapterCatalog:test.chapterCatalog,  //小说目录
-				calHeight:0
+				calHeight:0,
+				testChapters:test.content,
+				setStatus:false
 			}
 		},
 		methods: {
-			
+			changeSetStatus(){
+				this.setStatus = !this.setStatus
+			}
+		},
+		mounted(){
+			tool.calSurplusHeight({
+				pageID:this,
+				pos:'cal',
+				success: val => this.calHeight = val
+			})
 		},
 		onLoad(e){
 			console.log(e);
@@ -61,6 +81,10 @@
 
 .reading-bottom{
 	height: 200rpx;
+}
+
+.cal{
+	height: 0rpx;
 }
 
 </style>
