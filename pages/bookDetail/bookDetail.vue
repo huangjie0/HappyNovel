fd10f578904daaac5ab5f263a16f4ab89ef67c8b
huangjie4 <1364045672@qq.com>
2024-08-20 15:54:45 +0800

    修改

--- pages/bookDetail/bookDetail.vue
+++ pages/bookDetail/bookDetail.vue
@@ -1,7 +1,7 @@
 <template>
 	<view>
-		<PageTitle>图片详情</PageTitle>
-		<view class="book-details flex align-center py-2">
+		<PageTitle class="cla">图片详情</PageTitle>
+		<view class="book-details flex align-center py-2 cal">
 			<image src="@/static/Rebook/Rebook1.jpg" mode="widthFix" lazy-load class="flex-1 mx-2 rounded"></image>
 			<view class="flex-2 mx-2">
 				<view class="book-details-name">
@@ -16,18 +16,18 @@
 				</view>
 			</view>
 		</view>
-		<view class="shadow tab-container">
-			<TabTop :tab-arr="['详情','目录']" @getTabIndex="getTabIndex"></TabTop>
+		<view class="shadow">
+			<TabTop :tab-arr="['详情','目录']" @getTabIndex="getTabIndex" class="cal"></TabTop>
 			<!-- 具体内容 -->
 			<!-- 详情 -->
-			<scroll-view scroll-y v-if="tabIndex == 0" class="intro-container">
+			<scroll-view scroll-y v-if="tabIndex == 0" :style="{ height:`${calHeight}rpx` }">
 				<view>
 					<view class="py-2 flex justify-center text-light-black">——简介——</view>
 					<view class="px-2 font-lg intro">{{ synopsis }}</view>
 				</view>
 			</scroll-view>
 			<!-- 目录 -->
-			<scroll-view scroll-y v-else class="catalogue-container">
+			<scroll-view scroll-y v-else :style="{ height:`${calHeight}rpx`}">
 				<block v-for="(item,index) in chapterCatalog" :key="item.id">
 					<view class="p-2 text-ellipsis border-bottom" hover-class="bg-light" @tap="toReadingPage(item.id)">
 						{{ item.title }}
@@ -42,9 +42,11 @@
 	import PageTitle from '@/components/PageTitle.vue'
 	import TabTop from '@/components/TabTop.vue'
 	import test from '@/common/test'
+	import tool from '@/common/tool.js'
 	export default {
 		data(){
 			return {
+				calHeight:0,
 				tabIndex:0,
 				name:test.name,
 				author:test.author,
@@ -55,6 +57,13 @@
 		components:{
 			PageTitle,
 			TabTop
+		},
+		mounted(){
+			tool.calSurplusHeight({
+				pageID:this,
+				pos:'cal',
+				success:val => this.calHeight = val
+			})
 		},
 		methods:{
 			getTabIndex(index){
@@ -77,16 +86,8 @@
 		font-size: 45rpx;
 	}
 }
-.tab-container{
-	height:1210rpx;
-	.intro-container{
-		height: 1130rpx;
-		.intro{
-			line-height: 80rpx;
-		}
-	}
-	.catalogue-container{
-		height: 1130rpx;
-	}
+
+.intro{
+	line-height: 80rpx;
 }
 </style>
--- pages/reading/reading.vue
+++ pages/reading/reading.vue
@@ -10,10 +10,23 @@
 				<text class="px-2 font-sm text-ellipsis flex-1">章节：{{ chapterCatalog[0].title }}</text>
 			</view>
 		</view>
-		<!-- 设置结束 -->		
+		<!-- 设置结束 -->
+				
+		<!-- 目录开始 -->
+		<uniDrawer ref="drawerRef" :width="230">
+			<view class="chapter-selection flex align-center justify-center">章节选择</view>
+			<scroll-view scroll-y :style="{ height:`${calHeight - 80 }rpx` }" >
+				<block v-for="item in chapterCatalog" :key="item.id">
+					<view class="chapter-title px-1 py-2 text-ellipsis">
+						{{ item.title }}
+					</view>
+				</block>
+			</scroll-view>
+		</uniDrawer>
+		<!-- 目录结束 -->
 		<!-- 设置底部部分 -->
 		<view class="reading-bottom flex align-center fixed-bottom bg-white shadow font animated slideInUp" v-if="setStatus">
-			<view class="flex-1 flex flex-column align-center">
+			<view class="flex-1 flex flex-column align-center" @tap="showCatalogue">
 				<icon icon-id="icon-xueyuan-mulu" icon-size="55"></icon>
 				<view>目录</view>
 			</view>
@@ -32,9 +45,10 @@
 		</view>
 		<!-- 文本部分 -->
 		
-		<swiper class="px-2" :style="{ height:`${calHeight}rpx`}" @tap="changeSetStatus">
-			<swiper-item v-for="(item,index) in testChapters" :key="item.id">
+		<swiper class="px-2" :style="{ height:`${calHeight}rpx`}" @tap="changeSetStatus" :current="chapterIndex" @change="swiperChange">
+			<swiper-item v-for="(item,index) in loadedChapters" :key="item.id">
 				<scroll-view scroll-y :style="{ height:`${calHeight}rpx`}">
+					<uniLoadMore status="loading" v-if="!item.text"></uniLoadMore>
 					<rich-text :nodes="item.text"></rich-text>
 				</scroll-view>
 			</swiper-item>
@@ -46,19 +60,53 @@
 	import test from '@/common/test.js'
 	import tool from '@/common/tool.js'
 	import htmlParser from '@/common/html-parser.js'
+	import uniLoadMore from '@/components/uni-load-more/uni-load-more.vue'
+	import uniDrawer from '@/components/uni-drawer/uni-drawer.vue'
 	export default {
 		data() {
 			return {
 				novalName:test.name,  //小说姓名
 				chapterCatalog:test.chapterCatalog,  //小说目录
 				calHeight:0,
-				testChapters:test.content,
-				setStatus:false
+				loadedChapters:[], //已经加载的章节
+				setStatus:false,
+				chapterIndex:0 //当前章节的标识
 			}
 		},
+		components:{
+			uniLoadMore,
+			uniDrawer
+		},
 		methods: {
+			//预加载方法
+			preLoad(){
+				test.chapterCatalog.forEach(item => this.loadedChapters.push({
+					id:item.id,
+					title:item.title,
+					text:''
+				}))
+				console.log(this.loadedChapters);
+			},
+			changeIndex(index){
+				this.chapterIndex = index
+			},
+			swiperChange(e){
+				this.changeIndex(e.detail.current)
+				this.deplyLoad()
+			},
+			//延时加载
+			deplyLoad(){
+				let index = this.chapterIndex
+				if(this.loadedChapters[index].text == ''){
+					setTimeout(()=>this.loadedChapters[index].text = htmlParser(test.content[index].text),1000)
+				}
+			},
 			changeSetStatus(){
 				this.setStatus = !this.setStatus
+			},
+			showCatalogue(){
+				this.$refs.drawerRef.open()
+				this.changeSetStatus()
 			}
 		},
 		mounted(){
@@ -69,7 +117,8 @@
 			})
 		},
 		onLoad(e){
-			console.log(e);
+			this.preLoad()
+			this.deplyLoad()
 		}
 	}
 </script>
@@ -85,6 +134,13 @@
 
 .cal{
 	height: 0rpx;
+}
+
+.chapter-selection{
+	height: 80rpx;
+}
+.chapter-title{
+	
 }
 
 </style>
