83f30636872b64a8e64407f98804f2754e5641a8
huangjie4 <1364045672@qq.com>
2024-08-26 16:13:47 +0800

    修改

--- pages/collection/collection.vue
+++ pages/collection/collection.vue
@@ -1,20 +1,53 @@
 <template>
 	<view>
-		<CollectItem></CollectItem>
+		<SearchBox class="cal"></SearchBox>
+		<TabTop :tab-arr="['我的收藏','收听历史']" @getTabIndex="getTabIndex" class="cal"></TabTop>
+		<!-- 具体内容 -->
+		<!-- 我的收藏 -->
+		<scroll-view scroll-y v-if="tabIndex == 0" :style="{ height:`${calHeight}rpx` }">
+			111
+		</scroll-view>
+		<!-- 收听历史 -->
+		<scroll-view scroll-y v-else :style="{ height:`${calHeight}rpx`}">
+			222
+		</scroll-view>
+		
+		
+		
+		
+		<!-- <CollectItem></CollectItem> -->
 	</view>
 </template>
 
 <script>
+	import SearchBox from '@/components/SearchBox.vue'
+	import TabTop from '@/components/TabTop.vue'
 	import CollectItem from '@/components/CollectItem.vue'
+	import tool from '@/common/tool.js'
 	
 	export default {
 		data(){
 			return {
-				
+				tabIndex:0,
+				calHeight:0,
 			}
 		},
+		methods:{
+			getTabIndex(index){
+				this.tabIndex = index
+			},
+		},
+		mounted(){
+			tool.calSurplusHeight({
+				pageID:this,
+				pos:'cal',
+				success:val => this.calHeight = val
+			})
+		},
 		components:{
-			CollectItem
+			SearchBox,
+			TabTop,
+			CollectItem,
 		}
 	}
 </script>
