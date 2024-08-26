94ad2a5cbe43d573e6f62ec4e05b84a9032ee4f5
huangjie4 <1364045672@qq.com>
2024-08-26 16:13:33 +0800

    修改

--- components/CollectItem.vue
+++ components/CollectItem.vue
@@ -1,6 +1,16 @@
 <template>
 	<view>
-		223
+		<view class="books-container border-bottom flex align-center p-1">
+			<image :src="bookImgUrl" mode="aspectFill" class="books-image rounded mx-2"></image>
+			<view class="books-text flex-1">
+				<view class="py-1 font-md font-weight-bold">
+					{{ bookName }}
+				</view>
+				<view class="font text-light-black">
+					{{ bookSynopsis }}
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -11,10 +21,33 @@
 			return {
 				
 			};
+		},
+		props:{
+			bookImgUrl: {
+				type:String,
+				default:''
+			},
+			bookName:{
+				type:String,
+				default:''
+			},
+			bookSynopsis:{
+				type:String,
+				default:''
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style scope lang="scss">
+	.books-container{
+		height: 250rpx;
+		.books-image{
+			width: 220rpx;
+			height: 220rpx;
+		}
+		.books-text{
+			height: 100%;
+		}
+	}
 </style>
