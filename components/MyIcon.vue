838b6c3bfc649d8ca0ae7ec84ac980523ed2bd28
huangjie4 <1364045672@qq.com>
2024-08-27 15:57:21 +0800

    修改

---
+++ components/MyIcon.vue
@@ -0,0 +1,28 @@
+<template>
+	<!-- 图标组件封装 -->
+	<text class="iconfont" :class="[iconId,iconColor]" :style="{fontSize: `${iconSize}rpx`}"></text>
+</template>
+
+<script>
+	export default {
+		name:"Icon",
+		props:{
+			iconId:{
+				type:String,
+				default:''
+			},
+			iconColor:{
+				type:String,
+				default:'text-dark'
+			},
+			iconSize:{
+				type:String,
+				default:'45'
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
