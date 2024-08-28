593f2d7b204c46879fddc0e0c5331abc53edd29d
huangjie4 <1364045672@qq.com>
2024-08-28 11:04:47 +0800

    修改

--- common/filter.js
+++ common/filter.js
@@ -1,5 +1,8 @@
 export default {
 	formatTime(num){
+		if(!num && num !== 0){
+			return '00:00'
+		}
 		let divisionNum = Math.floor(num / 60), 
 		remainderNum = Math.floor(num % 60),
 		zero = (x) => '0'.repeat(2 - String(x).length);
